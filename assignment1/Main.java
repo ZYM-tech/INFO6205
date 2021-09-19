bc6cdc5d423ac769628a2d09be24b4df2e80041d
ZYM <yiming.zhang1024@gmail.com>
2021-09-19 00:01:19 -0700

    Question 4

--- assignment1/Main.java
+++ assignment1/Main.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args){
-        //Question 1 Test
+        //Question 1 Testcase
         int[] nums = new int[]{2,0,2,1,1,0};
         int[] sorted_nums  = Question_1.sortColor(nums);
         System.out.println("Question 1: "+Arrays.toString(sorted_nums));
 
 
-        //Question 2 Test
+        //Question 2 Testcase
         ArrayList<Question_2.Interval> meetings = new ArrayList<>();
         //add a meeting time, must be int
         meetings.add(new Question_2.Interval(7,10));
@@ -22,9 +22,14 @@ public class Main {
         System.out.println("Question 2: "+Question_2.Allmeeting(meetings));
 
 
-        //Question 3 Test
+        //Question 3 Testcase
         int[] nums_2n = new int[]{6,2,6,5,1,2};
         System.out.println("Question 3: "+Question_3.maxsum(nums_2n));
+
+
+        //Question 4 Testcase
+        int[] nums_sorted = new int[]{-7,-3,2,3,11};
+        System.out.println("Question 4: "+Arrays.toString(Question_4.sqnums(nums_sorted)));
     }
 
 
--- assignment1/Question_4.java
+++ assignment1/Question_4.java
@@ -1,2 +1,25 @@
+import java.util.Arrays;
+
 public class Question_4 {
+//use left and right pointers, compare the absolute value of left and right, bigger ones add to result array first.
+    public static int[] sqnums(int[] nums){
+        int i = 0;//left pointer
+        int j = nums.length-1;//right pointer
+        int a = nums.length-1;//result array pointer
+        int[] sqnums = new int[nums.length];
+
+        while(i<=j && a>=0){
+            //left > right
+            if(nums[i]+nums[j]<=0){
+                sqnums[a] = nums[i]*nums[i];
+                i++;
+            }//left < right
+            else if(nums[i]+nums[j]>0){
+                sqnums[a] = nums[j]*nums[j];
+                j--;
+            }
+            a--;
+        }
+        return sqnums;
+    }
 }
