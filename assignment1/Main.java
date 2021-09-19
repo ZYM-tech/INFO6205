421eb5e1cc5242220ac0aeb32e8f03e397f68b71
ZYM <yiming.zhang1024@gmail.com>
2021-09-18 20:36:03 -0700

    Question 2

--- assignment1/Main.java
+++ assignment1/Main.java
@@ -1,11 +1,25 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
+
 public class Main {
+
     public static void main(String[] args){
         //Question 1 Test
         int[] nums = new int[]{2,0,2,1,1,0};
         int[] sorted_nums  = Question_1.sortColor(nums);
-        System.out.println(Arrays.toString(sorted_nums));
+        System.out.println("Question 1: "+Arrays.toString(sorted_nums));
 
+
+        //Question 2 Test
+        ArrayList<Question_2.Interval> meetings = new ArrayList<>();
+        //add a meeting time, must be int
+        meetings.add(new Question_2.Interval(7,10));
+        meetings.add(new Question_2.Interval(2,4));
+
+        Question_2.Allmeeting(meetings);
+        System.out.println("Question 2: "+Question_2.Allmeeting(meetings));
     }
+
+
 }
---
+++ assignment1/Question_2.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Question_2 {
+
+    //Question 2: define Interval
+    public static class Interval {
+
+        public int start;
+        public int end;
+
+        public Interval(int start, int end){
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+
+    public static boolean Allmeeting(ArrayList<Interval> meetings) {
+        if(meetings == null || meetings.size() <= 1  )
+            return true;
+
+        //sort intervals(meetings) by start time
+        meetings.sort(new Comparator<>() {
+            @Override
+            public int compare(Interval x, Interval y) {
+                return Integer.compare(x.start, y.start);
+            }
+        });
+
+        //compare if these sorted meetings have time conflicts
+        for (int i = 1; i < meetings.size(); i++) {
+            Interval rear = meetings.get(i-1);
+            Interval current = meetings.get(i);
+            if (rear.end >= current.start || rear.start == current.start) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
